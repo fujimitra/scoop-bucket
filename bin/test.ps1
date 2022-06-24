bf7183eeea0287536d9cc0137d6955e66513149b
fujimitra <shachi.hoko.x@gmail.com>
2022-06-24 18:04:20 +0900

    Updated apps

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,5 +1,1 @@
-#Requires -Modules @{ ModuleName = 'Pester'; MaximumVersion = '4.99' }
-
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
-$result = Invoke-Pester "$PSScriptRoot\.." -PassThru
-exit $result.FailedCount
+. "$PSScriptRoot\..\test\bin\test.ps1"
